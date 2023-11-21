4d62f6b85842f296fc9cff884e8071997d4d2e36
Felipe Torres Cresto <felipe.t.cresto@gmail.com>
2023-11-21 16:26:32 -0300

    feat: create project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CrudListComponent } from './crud-exemple/crud-list/crud-list.component';
+import { CrudFormComponent } from './crud-exemple/crud-form/crud-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CrudListComponent,
+    CrudFormComponent
   ],
   imports: [
     BrowserModule,
